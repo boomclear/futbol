abe09a5767f7e87e752536fca7c6d64af8a2b158
Austin Woods <austinwoods99@gmail.com>
2023-04-21 14:23:20 -0400

    Complete count of teams, total games by team, best offence, and worst offence methods.

--- lib/league.rb
+++ lib/league.rb
@@ -5,4 +5,54 @@ class League
     @league_data = data
   end
   
+  def count_of_teams
+    teams = []
+    @league_data.each do |game|
+      teams << game[:team_id]
+    end
+    teams.uniq.length
+  end
+
+  def total_goals_by_team
+    goals = {}
+    @league_data.each do |game|
+      if !goals[game[:team_id]]
+        goals[game[:team_id]] = game[:goals].to_i
+      else
+        goals[game[:team_id]] += game[:goals].to_i
+      end
+    end
+    goals
+  end
+
+  def total_games_by_team
+    games = {}
+    @league_data.each do |game|
+      if !games[game[:team_id]]
+       games[game[:team_id]] = 1
+      else
+       games[game[:team_id]] += 1
+      end
+    end
+    games
+  end
+
+  def best_offence
+    offenses = {}
+    total_goals_by_team.each do |team, goals|
+      offenses[team] = goals.to_f / total_games_by_team[team]
+    end
+    best_average = offenses.values.max
+    offenses.key(best_average)
+  end
+
+  def worst_offence
+    offenses = {}
+    total_goals_by_team.each do |team, goals|
+      offenses[team] = (goals.to_f / total_games_by_team[team]).round(2)
+    end
+    best_average = offenses.values.min
+    offenses.key(best_average)
+  end
+
 end
--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -20,4 +20,39 @@ RSpec.describe League do
       expect(@league).to be_a(League)
     end
   end
+
+  describe "team amount" do
+    it "can count the number of teams" do 
+      expect(@league2.count_of_teams).to eq(14)
+    end
+  end
+  describe "team offenses and total games" do
+    it "can give total games by team" do 
+      expected = {
+        "3"=>2,
+        "6"=>2,
+        "5"=>3,
+        "17"=>4,
+        "16"=>3,
+        "9"=>1,
+        "8"=>1,
+        "30"=>1,
+        "26"=>1,
+        "19"=>1,
+        "24"=>2,
+        "2"=>2,
+        "52"=>1,
+        "28"=>1
+      }
+      expect(@league2.total_games_by_team).to eq(expected)
+    end
+
+    it "can give best offensive team" do 
+      expect(@league2.best_offence).to eq("8")
+    end
+
+    it "can give worst offensive team" do
+      expect(@league2.worst_offence).to eq("28")
+    end
+  end
 end
