2197f35f735386f5783e3bd00dc94853e68e96c5
Austin Woods <austinwoods99@gmail.com>
2023-04-22 14:03:32 -0400

    Add test for team_id_to_tea_name.

--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -49,4 +49,45 @@ RSpec.describe League do
       expect(@league.worst_offense).to eq("Los Angeles FC")
     end
   end
+  
+  describe "team id to team names" do 
+    it "has a hash containing team ID keys and team name values" do 
+      expected = {
+        "1"=>"Atlanta United",
+        "4"=>"Chicago Fire",
+        "26"=>"FC Cincinnati",
+        "14"=>"DC United",
+        "6"=>"FC Dallas",
+        "3"=>"Houston Dynamo",
+        "5"=>"Sporting Kansas City",
+        "17"=>"LA Galaxy",
+        "28"=>"Los Angeles FC",
+        "18"=>"Minnesota United FC",
+        "23"=>"Montreal Impact",
+        "16"=>"New England Revolution",
+        "9"=>"New York City FC",
+        "8"=>"New York Red Bulls",
+        "30"=>"Orlando City SC",
+        "15"=>"Portland Timbers",
+        "19"=>"Philadelphia Union",
+        "24"=>"Real Salt Lake",
+        "27"=>"San Jose Earthquakes",
+        "2"=>"Seattle Sounders FC",
+        "20"=>"Toronto FC",
+        "21"=>"Vancouver Whitecaps FC",
+        "25"=>"Chicago Red Stars",
+        "13"=>"Houston Dash",
+        "10"=>"North Carolina Courage",
+        "29"=>"Orlando Pride",
+        "52"=>"Portland Thorns FC",
+        "54"=>"Reign FC",
+        "12"=>"Sky Blue FC",
+        "7"=>"Utah Royals FC",
+        "22"=>"Washington Spirit FC",
+        "53"=>"Columbus Crew SC"
+      }
+      require 'pry'; binding.pry
+      expect(@league.team_id_to_team_name).to eq(expected)
+    end
+  end
 end
