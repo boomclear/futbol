c0f76d9ae9d030eae03f3db22b1df417e48ec505
Steven Stackhouse <stackmm@gmail.com>
2023-04-23 13:26:30 -0600

    add methods to stat_tracker for spec harness testing

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,14 +1,19 @@
 require "csv"
+require_relative "game"
+require_relative "league"
+require_relative "season"
 
 class StatTracker
-  attr_reader :games, :teams, :game_teams
+  attr_reader :games, :teams, :game_teams, :game, :league, :game_teams_data
+
   def initialize(locations)
     @games_data = CSV.read(locations[:games], headers: true, header_converters: :symbol)
     @teams_data = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
     @game_teams_data = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
-    # @game = Game.new(@games_data)
-    # @league = League.new(???)
-    # @season = Season.new(???)
+
+    @game = Game.new(@games_data)
+    @league = League.new(@games_data, @games_teams_data, @teams_data)
+    @season = Season.new(@games_data, @games_teams_data, @teams_data)
   end
 
   def self.from_csv(location)
@@ -16,211 +21,107 @@ class StatTracker
   end
 
   def highest_total_score
-    @games_data.map do |row|
-      row[:away_goals].to_i + row[:home_goals].to_i
-    end.max
+    @game.highest_total_score
   end
 
   def lowest_total_score
-    @games_data.map do |row|
-      row[:away_goals].to_i + row[:home_goals].to_i
-    end.min
+    @game.lowest_total_score
   end
 
+  # expect(@stat_tracker.percentage_home_wins).to eq 0.44
   def percentage_home_wins
-    game_count = 0
-    wins = 0
-    @game_teams_data.each do |game|
-      game_count += 1
-      if (game[:hoa] == "home" && game[:result] == "WIN") || (game[:hoa] == "away" && game[:result] == "LOSS")
-        wins += 1
-      end
-    end
-    percentage = wins / game_count.to_f
-    percentage = percentage.round(2)
+    @game.percentage_home_wins
   end
 
+  # expect(@stat_tracker.percentage_visitor_wins).to eq 0.36
   def percentage_visitor_wins
-    game_count = 0
-    wins = 0
-    @game_teams_data.each do |game|
-      game_count += 1
-      if (game[:hoa] == "away" && game[:result] == "WIN") || (game[:hoa] == "home" && game[:result] == "LOSS")
-        wins += 1
-      end
-    end
-    percentage = wins / game_count.to_f
-    percentage = percentage.round(2)
+    @game.percentage_visitor_wins
   end
 
   def percentage_ties
-    ties = 0
-    games = 0
-    @game_teams_data.each do |row|
-      games += 1
-      if row[:result] == "TIE"
-        ties += 1
-      end
-    end
-    total = ties.to_f / games
-    total.round(2)
+    @game.percentage_ties
   end
 
   def count_of_games_by_season
-    games_by_season = {}
-    @games_data.each do |game|
-      if games_by_season[game[:season]].nil?
-        games_by_season[game[:season]] = 1
-      else
-        games_by_season[game[:season]] += 1
-      end
-    end
-    games_by_season
+    @game.count_of_games_by_season
   end
 
   def average_goals_per_game
-    goals = []
-    @games_data.each do |game|
-      goals_sum = game[:away_goals].to_i + game[:home_goals].to_i
-      goals << goals_sum
-    end
-    total = goals.sum / goals.length.to_f
-    total.round(2)
+    @game.average_goals_per_game
   end
 
-  def count_of_goals_by_season
-    goals_by_season = {}
-    @games_data.each do |game|
-      if goals_by_season[game[:season]].nil?
-        goals_by_season[game[:season]] = game[:home_goals].to_i + game[:away_goals].to_i
-      else
-        goals_by_season[game[:season]] += game[:home_goals].to_i + game[:away_goals].to_i
-      end
-    end
-    goals_by_season
-  end
   def average_goals_by_season
-    average_goals = {}
-    count_of_goals_by_season.each do |season, goals|
-      average_goals[season] = (goals.to_f / count_of_games_by_season[season]).round(2)
-    end
-    average_goals
-  end
-  def create_season(season)
-    specific_season = []
-    @game_teams_data.each do |row|
-      specific_season << row if row[:game_id].start_with?(season.to_s[0,4])
-    end
-    specific_season
-  end
-  def team_accuracy(season)
-    team_shots_total = Hash.new(0)
-    team_goals_total = Hash.new(0)
-    @team_accuracies = Hash.new
-    create_season(season).each do |row|
-      shots = team_shots_total[row[:team_id]] += row[:shots].to_i
-      goals = team_goals_total[row[:team_id]] += row[:goals].to_i
-      team_accuracy = goals / shots.to_f
-      team_id = row[:team_id]
-      @team_accuracies[team_id] = team_accuracy
-    end
-  end
-  def most_accurate_team(season)
-    team_accuracy(season)
-    @teams_data.each do |team|
-      if team[:team_id] == @team_accuracies.key(@team_accuracies.values.max)
-        return name = team[:teamname]
-      end
-    end
-  end
-  def least_accurate_team(season)
-    team_accuracy(season)
-    @teams_data.each do |team|
-      if team[:team_id] == @team_accuracies.key(@team_accuracies.values.min)
-        return name = team[:teamname]
-      end
-    end
+    @game.average_goals_by_season
   end
 
-  def get_team_name(team_id)
-    @teams_data.each do |row|
-      return row[:teamname] if row[:team_id] == team_id
-    end
-  end
-
-  def most_tackles(season)
-    team_tackle_totals = Hash.new(0)
-
-    create_season(season).each do |row|
-      team_tackle_totals[row[:team_id]] += row[:tackles].to_i
-    end
-
-    team_most_tackles_id = team_tackle_totals.max_by do |team_id, tackles|
-      tackles
-    end
-
-    get_team_name(team_most_tackles_id[0])
-  end
-
-  # def fewest_tackles(season)
-  # end
-  
+  # expect(@stat_tracker.count_of_teams).to eq 32
   def count_of_teams
-    teams = []
-    @game_teams_data.each do |game|
-      teams << game[:team_id]
-    end
-    teams.uniq.length
-  end
-  def total_goals_by_team
-    goals = {}
-    @game_teams_data.each do |game|
-      if !goals[game[:team_id]]
-        goals[game[:team_id]] = game[:goals].to_i
-      else
-        goals[game[:team_id]] += game[:goals].to_i
-      end
-    end
-    goals
+    @league.count_of_teams
   end
 
-  def total_games_by_team
-    games = {}
-    @game_teams_data.each do |game|
-      if !games[game[:team_id]]
-        games[game[:team_id]] = 1
-      else
-        games[game[:team_id]] += 1
-      end
-    end
-    games
-  end
-
+  # expect(@stat_tracker.best_offense).to eq "Reign FC"
   def best_offense
-    offenses = {}
-    total_goals_by_team.each do |team, goals|
-      offenses[team] = goals.to_f / total_games_by_team[team]
-    end
-    best_average = offenses.values.max
-    team_id_to_team_name[offenses.key(best_average)]
+    @league.best_offense
   end
 
+  # expect(@stat_tracker.worst_offense).to eq "Utah Royals FC"
   def worst_offense
-    offenses = {}
-    total_goals_by_team.each do |team, goals|
-      offenses[team] = (goals.to_f / total_games_by_team[team]).round(2)
-    end
-    best_average = offenses.values.min
-    team_id_to_team_name[offenses.key(best_average)]
+    @league.worst_offense
   end
 
-  def team_id_to_team_name
-    teams = {}
-    @teams_data.each do |team|
-      teams[team[:team_id]] = team[:teamname]
-    end
-    teams
+  # expect(@stat_tracker.highest_scoring_visitor).to eq "FC Dallas"
+  def highest_scoring_visitor
+    @league.highest_scoring_visitor
   end
 
+  # expect(@stat_tracker.highest_scoring_home_team).to eq "Reign FC"
+  def highest_scoring_home_team
+    @league.highest_scoring_home_team
+  end
 
+  # expect(@stat_tracker.lowest_scoring_visitor).to eq "San Jose Earthquakes"
+  def lowest_scoring_visitor
+    @league.lowest_scoring_visitor
+  end
+
+  # expect(@stat_tracker.lowest_scoring_home_team).to eq "Utah Royals FC"
+  def lowest_scoring_home_team
+    @league.lowest_scoring_home_team
+  end
+
+  # expect(@stat_tracker.winningest_coach("20132014")).to eq "Claude Julien"
+  # expect(@stat_tracker.winningest_coach("20142015")).to eq "Alain Vigneault"
+  def winningest_coach(season)
+    @season.winningest_coach(season)
+  end
+
+  # expect(@stat_tracker.worst_coach("20132014")).to eq "Peter Laviolette"
+  # expect(@stat_tracker.worst_coach("20142015")).to eq("Craig MacTavish").or(eq("Ted Nolan"))
+  def worst_coach(season)
+    @season.worst_coach(season)
+  end
+
+  # expect(@stat_tracker.most_accurate_team("20132014")).to eq "Real Salt Lake"
+  # expect(@stat_tracker.most_accurate_team("20142015")).to eq "Toronto FC"
+  def most_accurate_team(season)
+    @season.most_accurate_team(season)
+  end
+
+  # expect(@stat_tracker.least_accurate_team("20132014")).to eq "New York City FC"
+  # expect(@stat_tracker.least_accurate_team("20142015")).to eq "Columbus Crew SC"
+  def least_accurate_team(season)
+    @season.least_accurate_team(season)
+  end
+
+  # expect(@stat_tracker.most_tackles("20132014")).to eq "FC Cincinnati"
+  # expect(@stat_tracker.most_tackles("20142015")).to eq "Seattle Sounders FC"
+  def most_tackles(season)
+    @season.most_tackles(season)
+  end
+
+  # expect(@stat_tracker.fewest_tackles("20132014")).to eq "Atlanta United"
+  # expect(@stat_tracker.fewest_tackles("20142015")).to eq "Orlando City SC"
+  def fewest_tackles(season)
+    @season.fewest_tackles(season)
+  end
 end
